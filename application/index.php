a9a453175f82550ac15bd47387d20db7781539d0
Bedram Tamang <tmgbedu@gmail.com>
2024-12-14 14:19:56 -0800

    feat: mysql configured with kubernetes :db:

--- application/index.php
+++ application/index.php
@@ -1,3 +1,15 @@
 <?php
+$servername = "mysql";
+$username   = "root";
+$password   = "12345678";
+$database   = "jh";
+$port       = 4406;
 
-echo "hello what are you doing ?".gethostname();
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database, $port);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: ".$conn->connect_error);
+}
+echo "Connected successfully!";
---
+++ bin/deploy.sh
@@ -0,0 +1,18 @@
+kubectl create secret generic env-secret --from-env-file=./application/.env --dry-run=client -o yaml > k8s/env-secret.yaml
+kubectl apply -f k8s/env-secret.yaml
+
+# Mysql
+kubectl apply -f k8s/mysql-pvc.yaml
+kubectl apply -f k8s/mysql-deployment.yaml
+kubectl apply -f k8s/mysql-service.yaml
+
+# PHP
+kubectl apply -f k8s/php-deployment.yaml
+kubectl apply -f k8s/php-service.yaml
+
+# Nginx
+kubectl apply -f k8s/nginx-deployment.yaml
+kubectl apply -f k8s/nginx-service.yaml
+
+# Restart all the deployment
+kubectl rollout restart deployment
